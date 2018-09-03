213bfcb57fc0f96a2425ded9eb6821fc2395b764
baixiaojian <15101661380@163.com>
2018-09-03 22:31:53 +0800

    update ..

--- webhook.js
+++ webhook.js
@@ -1,7 +1,7 @@
 const http       = require('http')
 const ip         = require('ip');
 const { spawn }  = require('child_process');
-const intranetIp = '172.31.125.176';
+const localhostIp = '47.75.198.92';
 const runCommand = (cmd, args, callback) => {
   const child = spawn(cmd, args);
   let response = '';
@@ -12,10 +12,10 @@ const runCommand = (cmd, args, callback) => {
 };
 
 http.createServer(function (req, res) {
-  if(ip.address() === intranetIp) {
+  if(ip.address() === localhostIp) {
     // runCommand('sh', [ './auto_deploy.sh' ], txt => {
       // console.log(txt);
-      res.end(`Update end ${ip.address()}`);
+      res.end(`Update end ${ip.address()} ===== ${req.connection.remoteAddress}`);
     // });
   } else {
     res.end(`No permissions ${ip.address()}`);
