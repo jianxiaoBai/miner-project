1f2cdb24a8b5123871152bfab6d8a77ceeaa73d8
baixiaojian <15101661380@163.com>
2018-09-03 22:04:04 +0800

    update webhook

--- config/index.js
+++ config/index.js
@@ -10,12 +10,10 @@ let apiUrl
 switch (process.env.NODE_ENV) {
   case 'development':
     apiUrl = 'http://127.0.0.1:8011'
-
     // apiUrl = 'http://47.75.198.92:8011'
     break
   case 'production':
     // apiUrl = 'http://45.40.243.125:8011'
-
     apiUrl = 'http://47.75.198.92:8011'
     break
 }
