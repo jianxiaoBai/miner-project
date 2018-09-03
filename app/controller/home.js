03bd41556427d74b020f677f75dd7d58c84babe5
baixiaojian <15101661380@163.com>
2018-09-03 17:46:47 +0800

    test ip

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,10 +3,12 @@
 const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
+    // debugger;
     this.ctx.body = {
       code: 0,
       message: '请求成功了',
       data: null,
+      ip: this.ctx.ip
     };
   }
 }
