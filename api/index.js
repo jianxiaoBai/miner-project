9567338a40b0a68872a03eb411137cc594e75290
baixiaojian <15101661380@163.com>
2018-09-09 01:24:36 +0800

    🚲 update http index

--- api/index.js
+++ api/index.js
@@ -1,85 +1,22 @@
 import http from './http.js'
 
-const apiCaptcha = (options) => {
-  return http.get('captcha', options);
-}
-const apiSendSms = (options) => {
-  return http.get('sendSms', options);
-}
-const apiLogin = (options) => {
-  return http.post('login', options);
-}
-const apiSign = (options) => {
-  return http.post('sign', options);
-}
-const aaa = (options) => {
-  return http.post('aaa', options);
-}
-const apiOkex = (options) => {
-  return http.post('okex', options);
-}
-const apiPayInfo = (options) => {
-  return http.post('payInfo', options);
-}
-const apiOrderForm = (options) => {
-  return http.get('orderForm', options);
-}
-const apiCheckAddr = (options) => {
-  return http.get('checkAddr', options);
-}
-const apiAuthAddr = (options) => {
-  return http.post('authAddr', options);
-}
-const apiPower = (options) => {
-  return http.get('power', options);
-}
-const apiUser = (options) => {
-  return http.get('user', options);
-}
-const apiRecord = (options) => {
-  return http.get('record', options);
-}
-const apiDelOrder = (options) => {
-  return http.delete('order', options);
-}
-const apiAsset = (options) => {
-  return http.get('asset', options);
-}
-const apiBuy = (options) => {
-  return http.post('buy', options);
-}
-const apiSell = (options) => {
-  return http.post('sell', options);
-}
-const apiCancelSell = (options) => {
-  return http.post('cancelSell', options);
-}
-const apiShopList = (options) => {
-  return http.get('shopList', options);
-}
-const apiShopDetail = (options) => {
-  return http.get('shopDetail', options);
-}
-
-export  {
-  apiCaptcha,
-  apiSendSms,
-  apiLogin,
-  apiSign,
-  aaa,
-  apiOkex,
-  apiPayInfo,
-  apiOrderForm,
-  apiCheckAddr,
-  apiAuthAddr,
-  apiPower,
-  apiUser,
-  apiRecord,
-  apiDelOrder,
-  apiAsset,
-  apiBuy,
-  apiSell,
-  apiCancelSell,
-  apiShopList,
-  apiShopDetail
-}
+export const apiCaptcha    = o => (http.get('captcha', o))
+export const apiSendSms    = o => (http.get('sendSms', o))
+export const apiLogin      = o => (http.post('login', o))
+export const apiSign       = o => (http.post('sign', o))
+export const aaa           = o => (http.post('aaa', o))
+export const apiOkex       = o => (http.post('okex', o))
+export const apiPayInfo    = o => (http.post('payInfo', o))
+export const apiOrderForm  = o => (http.get('orderForm', o))
+export const apiCheckAddr  = o => (http.get('checkAddr', o))
+export const apiAuthAddr   = o => (http.post('authAddr', o))
+export const apiPower      = o => (http.get('power', o))
+export const apiUser       = o => (http.get('user', o))
+export const apiRecord     = o => (http.get('record', o))
+export const apiDelOrder   = o => (http.delete('order', o))
+export const apiAsset      = o => (http.get('asset', o))
+export const apiBuy        = o => (http.post('buy', o))
+export const apiSell       = o => (http.post('sell', o))
+export const apiCancelSell = o => (http.post('cancelSell', o))
+export const apiShopList   = o => (http.get('shopList', o))
+export const apiShopDetail = o => (http.get('shopDetail', o))
