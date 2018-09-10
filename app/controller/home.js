8743966bc02441657281a1b6efa650c5f9c6f2b6
baixiaojian <15101661380@163.com>
2018-09-10 21:53:37 +0800

    quick restart

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,7 +3,6 @@
 const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
-    // debugger;
     this.ctx.body = {
       code: 0,
       message: '请求成功了',
