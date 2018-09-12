afce4ed3749c593536956849c1c18f28faa7b147
baixiaojian <15101661380@163.com>
2018-09-12 10:22:35 +0800

    🎧 update base url config

--- config/index.js
+++ config/index.js
@@ -14,7 +14,8 @@ switch (process.env.NODE_ENV) {
     break
   case 'production':
     // apiUrl = 'http://45.40.243.125:8011'
-    apiUrl = 'http://47.75.198.92:8011'
+    // apiUrl = 'http://47.75.198.92:8011'
+    apiUrl = 'https://www.ihashpower.pro/'
     break
 }
 
