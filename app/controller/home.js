814f46d68fcb6fee2cf8c0d0f44c86d1b841f7bc
baixiaojian <15101661380@163.com>
2018-09-14 11:25:34 +0800

    test api_dev branch

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const nodemailer = require('nodemailer');
 class HomeController extends Controller {
   async index() {
     this.ctx.body = {
